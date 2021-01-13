99fc87ee9824b085462cddfd12742b27a8bce717
zhbing <zhbing310@163.com>
2021-01-13 19:52:57 +0800

    第一次添加到缓冲区

---
+++ src/main/java/com/bgg/GitTest.java
@@ -0,0 +1,15 @@
+package com.bgg;
+
+/**
+ * ClassName: GitTest <br/>
+ * Description: <br/>
+ * date: 2021/1/13 19:45<br/>
+ *
+ * @author BGG<br />
+ * @since JDK 1.8
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hahhaha");
+    }
+}
