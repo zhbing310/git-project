30094800d0441301530e08ded5bdef1563995713
zhbing310 <77379147+zhbing310@users.noreply.github.com>
2021-01-13 20:14:26 +0800

    Update GitTest.java

--- src/main/java/com/bgg/GitTest.java
+++ src/main/java/com/bgg/GitTest.java
@@ -12,5 +12,8 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hahhaha");
         System.out.println("我修改了");
+        
+        System.out.println("修改的真棒");
+
     }
 }
